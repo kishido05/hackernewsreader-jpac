7ce608bb99a0b8b9d953e498766f257f77b74c39
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-07-19 11:51:14 +0800

    Create Fragments for Home and Detail screen

--- HackerNews/app/src/main/java/com/jpac/hackernews/MainActivity.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/MainActivity.java
@@ -1,17 +1,17 @@
 package com.jpac.hackernews;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
