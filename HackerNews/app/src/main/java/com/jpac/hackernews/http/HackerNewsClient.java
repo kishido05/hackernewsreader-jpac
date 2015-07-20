c73479fdee7814dc65e57e14a57f30bcffb9ce07
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-07-20 12:54:38 +0800

    Add Executor for Request Caching

--- HackerNews/app/src/main/java/com/jpac/hackernews/http/HackerNewsClient.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/http/HackerNewsClient.java
@@ -3,20 +3,28 @@ package com.jpac.hackernews.http;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
 
 public class HackerNewsClient {
 
+    public static final int N_THREADS = 50;
+    
     private static HackerNewsApi hackerNewsService;
 
     public static HackerNewsApi getHackerNewsClient() {
         if (hackerNewsService == null) {
             Gson gson = new GsonBuilder().create();
 
+            ExecutorService executorService = Executors.newFixedThreadPool(N_THREADS);
+
             RestAdapter restAdapter = new RestAdapter.Builder()
                     .setEndpoint(HackerNewsApi.END_POINT)
                     .setConverter(new GsonConverter(gson))
+                    .setExecutors(executorService, executorService)
                     .build();
 
             hackerNewsService = restAdapter.create(HackerNewsApi.class);
