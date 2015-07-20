40a4f71a640d46b1c36c34952ffe5916370329eb
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-07-20 17:30:26 +0800

    Add detail fragment/activity

--- HackerNews/app/src/main/java/com/jpac/hackernews/DetailActivity.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/DetailActivity.java
@@ -1,6 +1,14 @@
 package com.jpac.hackernews;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
--- HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
@@ -1,7 +1,19 @@
 package com.jpac.hackernews;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DetailFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        return rootView;
+    }
 }
