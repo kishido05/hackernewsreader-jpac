aab6e2c7e0296a6159c5868835008e6e1d873378
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-07-20 19:40:06 +0800

    Apply to Details Fragment

--- HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
@@ -2,15 +2,21 @@ package com.jpac.hackernews;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.jpac.hackernews.data.Comments;
+import com.jpac.hackernews.data.CommentsAdapter;
 import com.jpac.hackernews.data.News;
 import com.jpac.hackernews.http.HackerNewsClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -18,9 +24,16 @@ import retrofit.client.Response;
 
 public class DetailFragment extends ListFragment {
 
+    private CommentsAdapter commentsAdapter;
+
     private News news;
 
+    private int commentsCount;
+    private int downloadCount;
 
+    private SwipeRefreshLayout swipe;
+
+    private List<Comments> commentsList;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +44,20 @@ public class DetailFragment extends ListFragment {
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        swipe.post(new Runnable() {
+            @Override
+            public void run() {
+                swipe.setRefreshing(true);
+                downloadTopComments();
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,6 +66,22 @@ public class DetailFragment extends ListFragment {
         if (news != null) {
             setDetails(rootView);
         }
+
+        ListView list = (ListView) rootView.findViewById(android.R.id.list);
+
+        commentsAdapter = new CommentsAdapter(getActivity());
+        commentsList = new ArrayList<Comments>();
+
+        list.setAdapter(commentsAdapter);
+        list.setEmptyView(rootView.findViewById(android.R.id.empty));
+
+        swipe = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                downloadTopComments();
+            }
+        });
 
         return rootView;
     }
@@ -54,5 +97,102 @@ public class DetailFragment extends ListFragment {
         date.setText(news.getTime());
         points.setText(news.getScore());
 
+    }
+
+    private void downloadTopComments() {
+        if (news == null || news.getKids() == null) {
+            displayComments();
+            return;
+        }
+
+        String[] ids = news.getKids();
+        int len = ids.length;
+
+        // make sure that only the top 10 comments will be downloaded
+        len = len > 10 ? 10 : len;
+
+        commentsCount = len;
+
+        for (int i=0; i<len; i++) {
+            downloadComment(ids[i]);
+        }
+    }
+
+    private void downloadReply(final News parent) {
+        String[] ids = parent.getKids();
+
+        if (ids != null && ids.length > 0) {
+            HackerNewsClient.getHackerNewsClient().getDetail(ids[0], new Callback<News>() {
+                @Override
+                public void success(News news, Response response) {
+                    Comments comment = new Comments();
+                    comment.setComment(parent);
+                    if (news != null) {
+                        comment.setReply(news);
+                    }
+
+                    downloadCount++;
+
+                    commentsList.add(comment);
+
+                    if (downloadCount >= commentsCount) {
+                        displayComments();
+                    }
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    downloadCount++;
+
+                    if (downloadCount >= commentsCount) {
+                        displayComments();
+                    }
+                }
+            });
+        } else {
+            Comments comment = new Comments();
+            comment.setComment(parent);
+
+            downloadCount++;
+
+            commentsList.add(comment);
+
+            if (downloadCount >= commentsCount) {
+                displayComments();
+            }
+        }
+    }
+
+    private void downloadComment(String id) {
+        HackerNewsClient.getHackerNewsClient().getDetail(id, new Callback<News>() {
+
+            @Override
+            public void success(News news, Response response) {
+                downloadReply(news);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                downloadCount++;
+
+                if (downloadCount >= commentsCount) {
+                    displayComments();
+                }
+            }
+        });
+    }
+
+    private void displayComments() {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                commentsAdapter.add(commentsList);
+                commentsAdapter.notifyDataSetChanged();
+                swipe.setRefreshing(false);
+
+                commentsCount = 0;
+                downloadCount = 0;
+            }
+        });
     }
 }
