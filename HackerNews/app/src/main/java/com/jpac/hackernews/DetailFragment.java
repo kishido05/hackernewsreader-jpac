d9de0c4cca7752235f76d3724430ae50bdf9cd0f
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-07-26 20:31:19 +0800

    Clear comments after loading

--- HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
+++ HackerNews/app/src/main/java/com/jpac/hackernews/DetailFragment.java
@@ -137,6 +137,8 @@ public class DetailFragment extends Fragment {
             return;
         }
 
+        commentsList.clear();
+
         String[] ids = news.getKids();
         int len = ids.length;
 
